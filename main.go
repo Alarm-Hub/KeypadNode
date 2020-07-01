fa296eaa9f5b598eeae8db2bff58e456b74e6c15
Daniel Bacher <mail@phill93.de>
2020-07-01 18:55:12 +0200

    use wiegand code

--- main.go
+++ main.go
@@ -4,23 +4,25 @@ import (
 	"flag"
 	"fmt"
 	"github.com/Phill93/DoorManager/version"
-	
+	"github.com/Phill93/DoorManager/wiegand"
+	"time"
 )
 
 func main() {
 
-    
 	versionFlag := flag.Bool("version", false, "Version")
 	flag.Parse()
 
 	if *versionFlag {
 		fmt.Println("Build Date:", version.BuildDate)
-        fmt.Println("Git Commit:", version.GitCommit)
-        fmt.Println("Version:", version.Version)
-        fmt.Println("Go Version:", version.GoVersion)
-        fmt.Println("OS / Arch:", version.OsArch)
+		fmt.Println("Git Commit:", version.GitCommit)
+		fmt.Println("Version:", version.Version)
+		fmt.Println("Go Version:", version.GoVersion)
+		fmt.Println("OS / Arch:", version.OsArch)
 		return
 	}
 	fmt.Println("Hello.")
-    
+	wiegand.InitGpio()
+	time.Sleep(60 * time.Second)
+	defer wiegand.CleanGpios()
 }
