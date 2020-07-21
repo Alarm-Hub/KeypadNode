dcbd11df5048b492c60d099c3a2a0f9b2b55fede
Daniel Bacher <mail@phill93.de>
2020-07-21 16:46:38 +0200

    add code handling

--- main.go
+++ main.go
@@ -3,18 +3,46 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/Phill93/DoorManager/config"
 	"github.com/Phill93/DoorManager/log"
 	"github.com/Phill93/DoorManager/version"
 	"github.com/Phill93/DoorManager/wiegand"
 	"time"
 )
 
+func checkTimeout(timestamp time.Time, timeout int) bool {
+	now := time.Now()
+	offset := now.Sub(timestamp)
+	if offset.Seconds() > float64(timeout) {
+		log.Infof("timeout reached! %d", offset.Seconds())
+		return true
+	} else {
+		log.Infof("timeout not reached! %d", offset.Seconds())
+		return false
+	}
+}
+
 func handleEvents(events chan wiegand.Event) {
+	var code []string
+	var timestamp time.Time
 	for {
 		e := <-events
 		switch e.Type {
 		case "key":
 			log.Infof("Received key %s", e.Value)
+			if e.Value != "ENT" && e.Value != "ESC" {
+				if len(code) == 0 {
+					timestamp = time.Now()
+					log.Info("Recived key %v", timestamp)
+				}
+				code = append(code, e.Value)
+			} else if e.Value == "ENT" {
+				log.Infof("code is %s", code)
+				code = nil
+			} else if e.Value == "ESC" || checkTimeout(timestamp, 2) {
+				log.Info("clear code buffer")
+				code = nil
+			}
 		case "card":
 			log.Infof("Received card id %s", e.Value)
 		}
@@ -23,6 +51,7 @@ func handleEvents(events chan wiegand.Event) {
 
 func main() {
 	versionFlag := flag.Bool("version", false, "Version")
+	c := config.Config()
 	flag.Parse()
 	if *versionFlag {
 		fmt.Println("Build Date:", version.BuildDate)
@@ -35,6 +64,7 @@ func main() {
 
 	events := make(chan wiegand.Event, 1)
 
+	fmt.Print(c.Get("Test"))
 	defer wiegand.CleanGpios()
 	go wiegand.InitGpio(events)
 	go handleEvents(events)
