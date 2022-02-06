73310d9920803f49708cac649c7f20d19c461528
Daniel Bacher <mail@phill93.de>
2022-02-06 17:55:08 +0100

    backup

--- alarm/rpi.go
+++ alarm/rpi.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Phill93/DoorManager/config"
 	"github.com/Phill93/DoorManager/log"
 	"github.com/warthog618/gpiod"
+  "time"
 )
 
 var c *gpiod.Chip
@@ -13,11 +14,23 @@ var c *gpiod.Chip
 func (a *Alarm) Watch() {
 	cfg := config.Config()
 	c, _ = gpiod.NewChip("gpiochip0", gpiod.WithConsumer("AlarmWatcher"))
-	c.RequestLine(cfg.GetInt("pin_alarm"), gpiod.WithFallingEdge(a.alarm))
+	l, err := c.RequestLine(cfg.GetInt("pin_alarm"), gpiod.AsInput)
+	if err != nil {
+    panic(err)
+  }
 	log.Debug("Watching for Alarm")
+	for {
+	  if i, _ := l.Value(); i == 0 {
+	    time.Sleep(time.Second * 2)
+	    if i, _ := l.Value(); i == 0 {
+	      a.alarm()
+      }
+    }
+    time.Sleep(time.Millisecond * 100)
+  }
 }
 
-func (a *Alarm) alarm(evt gpiod.LineEvent) {
+func (a *Alarm) alarm() {
 	log.Info("Received alarm!")
 	a.Emit("alarm", "true")
 }
